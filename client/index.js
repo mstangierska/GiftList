354dfcd0c32fb309c179dd578d3a6c216c4fbcb0
Marta.Stangierska <marta.stangierska@bisih.org>
2024-04-28 16:01:50 +0200

    Alchemy University W2 Challenge

--- client/index.js
+++ client/index.js
@@ -1,14 +1,21 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const prompt = require('prompt-sync')({sigint:true})
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+  let name = prompt("What is your name? ");
+  var index = niceList.indexOf(name)
+  const merkle = new MerkleTree(niceList)
+
+  const proof = merkle.getProof(index)
+  const { data: gift } = await axios.post(`${serverUrl}/gift`,
+  {
+    'proof': proof,
+    'name': name
   });
 
   console.log({ gift });
